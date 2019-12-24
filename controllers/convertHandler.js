0a930c804e3e88e5f7f96fa2acbbaa8640174d63
Glitch (river-manchego) <none>
2019-12-24 19:47:49 +0000

    💒🎮 Checkpoint ./controllers/convertHandler.js:6079345/17

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -10,7 +10,7 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
    
-    var result =  input.match(/\d+[^\//${1,}]\d+/);//https://stackoverflow.com/questions/21443364/regex-to-match-integers-decimals-and-fractions
+    var result =  input.match(/\d+\///\d+/ig);//https://stackoverflow.com/questions/21443364/regex-to-match-integers-decimals-and-fractions
        // var result =  input.match(/\d+[\/\d.]*|\d/);
     //input.match(/\d+\.?\d+\/?\d+\.?\d+/); my effort       
      return result;
