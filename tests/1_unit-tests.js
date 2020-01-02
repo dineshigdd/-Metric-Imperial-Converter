72100b36ec39abb4253410d2f7a8a764e8f6e249
Glitch (river-manchego) <none>
2020-01-02 06:30:32 +0000

    🐣🌁 Checkpoint ./tests/1_unit-tests.js:6079345/1740

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -59,7 +59,9 @@ suite('Unit Tests', function(){
     test('For Each Valid Unit Inputs', function(done) {
       var input = ['gal','l','mi','km','lbs','kg','GAL','L','MI','KM','LBS','KG'];
       input.forEach(function(ele) {       
-        assert.equal(convertHandler.getUnit('L'));
+        assert.equal(convertHandler.getUnit('L'),'L');          
+
+        
       });
       done();
     });
