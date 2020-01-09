a99413467e971d23f7f31705366993cfb756071e
Glitch (river-manchego) <none>
2020-01-09 19:13:27 +0000

    💳💐 Checkpoint ./tests/2_functional-tests.js:6079345/55

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -42,7 +42,7 @@ suite('Functional Tests', function() {
             assert.equal(res.body.initNum, 32);
             assert.equal(res.body.initUnit, 'g');
             assert.approximately(res.body.returnNum,32);
-            assert.equal(res.body.returnUnit, 'invalid unit');
+            assert.equal(res.text, 'invalid unit');
             done();
           });        
       });
